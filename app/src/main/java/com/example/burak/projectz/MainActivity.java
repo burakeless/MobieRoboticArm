4e7cd94d625e732ffd33348a03d5a30cbf159310
Burak Keles <kelesburak13@outlook.com>
2019-01-11 08:37:53 +0300

    Android App

--- app/src/main/java/com/example/burak/projectz/MainActivity.java
+++ app/src/main/java/com/example/burak/projectz/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.burak.projectz;
 
+import android.annotation.SuppressLint;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
@@ -12,8 +13,8 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.ImageButton;
-import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
@@ -26,7 +27,7 @@ import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
-    private final String DEVICE_ADDRESS = "98:D3:31:F5:20:90"; //MAC Address of Bluetooth Module
+    private final String DEVICE_ADDRESS = "98:D3:34:90:4B:1A"; //MAC Address of Bluetooth Module
     private final UUID PORT_UUID = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
 
     private BluetoothDevice device;
@@ -35,11 +36,10 @@ public class MainActivity extends AppCompatActivity {
 
     String command;
 
-    Button forward_btn, back_btn, right_btn, left_btn, stop_btn, conn_btn, up_btn, down_btn;
-    ToggleButton claw_btn;
+    Button forward_btn, back_btn, right_btn, left_btn, stop_btn, conn_btn, open_btn, close_btn, scan_btn;
     ImageButton mic_btn;
-    TextView voiceCommand;
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,7 +47,6 @@ public class MainActivity extends AppCompatActivity {
 
         conn_btn = (Button) findViewById(R.id.conn);
         mic_btn = (ImageButton) findViewById(R.id.mic);
-        voiceCommand = (TextView) findViewById(R.id.voiceCommand_txt);
 
         forward_btn = (Button) findViewById(R.id.forward);
         back_btn = (Button) findViewById(R.id.back);
@@ -55,11 +54,11 @@ public class MainActivity extends AppCompatActivity {
         left_btn = (Button) findViewById(R.id.left);
         stop_btn = (Button) findViewById(R.id.stop);
 
-        up_btn = (Button) findViewById(R.id.up);
-        down_btn = (Button) findViewById(R.id.down);
-        claw_btn = (ToggleButton) findViewById(R.id.claw);
+        open_btn = (Button) findViewById(R.id.open);
+        close_btn = (Button) findViewById(R.id.close);
+        scan_btn = (Button) findViewById(R.id.scanon);
 
-        //Voice Command Button code
+
 
         mic_btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -69,9 +68,12 @@ public class MainActivity extends AppCompatActivity {
                 intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.ENGLISH);
                 try{
                     startActivityForResult(intent,200);
-                }catch (ActivityNotFoundException a){
+                }
+                catch (ActivityNotFoundException a){
                     Toast.makeText(getApplicationContext(),"Intent problem", Toast.LENGTH_SHORT).show();
                 }
+
+
             }
         });
 
@@ -83,7 +85,7 @@ public class MainActivity extends AppCompatActivity {
                 if (event.getAction() == MotionEvent.ACTION_DOWN){
                     command = "0";
                     try{
-                        outputStream.write(command.getBytes()); //transmits the value of command to the bluetooth module
+                        outputStream.write(command.getBytes());
                     }
                     catch (IOException e){
                         e.printStackTrace();
@@ -102,14 +104,14 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        //OnTouchListener code for the forward button (button long press)
+
             forward_btn.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 if (event.getAction() == MotionEvent.ACTION_DOWN){
                     command = "1";
                     try{
-                        outputStream.write(command.getBytes()); //transmits the value of command to the bluetooth module
+                        outputStream.write(command.getBytes());
                     }
                     catch (IOException e){
                         e.printStackTrace();
@@ -128,14 +130,14 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        //OnTouchListener code for the back button (button long press)
+
             back_btn.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 if (event.getAction() == MotionEvent.ACTION_DOWN){
                     command = "2";
                     try{
-                        outputStream.write(command.getBytes()); //transmits the value of command to the bluetooth module
+                        outputStream.write(command.getBytes());
                     }
                     catch (IOException e){
                         e.printStackTrace();
@@ -154,14 +156,14 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        //OnTouchListener code for the back button (button long press)
+
             left_btn.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 if (event.getAction() == MotionEvent.ACTION_DOWN){
                     command = "3";
                     try{
-                        outputStream.write(command.getBytes()); //transmits the value of command to the bluetooth module
+                        outputStream.write(command.getBytes());
                     }
                     catch (IOException e){
                         e.printStackTrace();
@@ -180,14 +182,14 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        //OnTouchListener code for the back button (button long press)
+
         right_btn.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 if (event.getAction() == MotionEvent.ACTION_DOWN){
                     command = "4";
                     try{
-                        outputStream.write(command.getBytes()); //transmits the value of command to the bluetooth module
+                        outputStream.write(command.getBytes());
                     }
                     catch (IOException e){
                         e.printStackTrace();
@@ -205,6 +207,101 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
+
+
+        open_btn.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN){
+                    command = "5";
+                    try{
+                        outputStream.write(command.getBytes());
+                    }
+                    catch (IOException e){
+                        e.printStackTrace();
+                    }
+                }
+                else if(event.getAction() == MotionEvent.ACTION_UP){
+                    command = "10";
+                    try{
+                        outputStream.write(command.getBytes());
+                    }
+                    catch(IOException e){
+                        e.printStackTrace();
+                    }
+                }
+                return false;
+            }
+        });
+
+        close_btn.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN){
+                    command = "6";
+                    try{
+                        outputStream.write(command.getBytes());
+                    }
+                    catch (IOException e){
+                        e.printStackTrace();
+                    }
+                }
+                else if(event.getAction() == MotionEvent.ACTION_UP){
+                    command = "10";
+                    try{
+                        outputStream.write(command.getBytes());
+                    }
+                    catch(IOException e){
+                        e.printStackTrace();
+                    }
+                }
+                return false;
+            }
+        });
+
+
+//        scan_btn.setOnTouchListener(new View.OnTouchListener() {
+//            @Override
+//            public boolean onTouch(View v, MotionEvent event) {
+//                if (event.getAction() == MotionEvent.ACTION_DOWN){
+//                    command = "7";
+//                    try{
+//                        outputStream.write(command.getBytes());
+//                    }
+//                    catch (IOException e){
+//                        e.printStackTrace();
+//                    }
+//                }
+//                else if(event.getAction() == MotionEvent.ACTION_UP){
+//                    command = "0";
+//                    try{
+//                        outputStream.write(command.getBytes());
+//                    }
+//                    catch(IOException e){
+//                        e.printStackTrace();
+//                    }
+//                }
+//                return false;
+//            }
+//        });
+
+        scan_btn.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                command = "8";
+                try{
+                    outputStream.write(command.getBytes());
+                }
+                catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+        });
+
+
+
+
+
 
         conn_btn.setOnClickListener(new OnClickListener() {
             @Override
@@ -264,7 +361,7 @@ public class MainActivity extends AppCompatActivity {
         }
         if(connected) {
             try {
-                outputStream = socket.getOutputStream(); //gets the output stream of the socket
+                outputStream = socket.getOutputStream();
             }
             catch(IOException e) {
                 e.printStackTrace();
@@ -284,7 +381,91 @@ public class MainActivity extends AppCompatActivity {
         if(requestCode == 200){
             if(resultCode == RESULT_OK && data != null){
                 ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-                voiceCommand.setText(result.get(0));
+
+
+
+                if (result.get(0).equalsIgnoreCase("dur")){
+                    command = "0";
+                    try{
+                        outputStream.write(command.getBytes());
+                    }
+                    catch (IOException e){
+                        e.printStackTrace();
+                    }
+                }
+
+                else if (result.get(0).equalsIgnoreCase("ileri")){
+                    command = "1";
+                    try{
+                        outputStream.write(command.getBytes());
+                    }
+                    catch (IOException e){
+                        e.printStackTrace();
+                    }
+                }
+
+                else if (result.get(0).equalsIgnoreCase("geri")){
+                    command = "2";
+                    try{
+                        outputStream.write(command.getBytes());
+                    }
+                    catch (IOException e){
+                        e.printStackTrace();
+                    }
+                }
+
+                else if (result.get(0).equalsIgnoreCase("sol")){
+                    command = "3";
+                    try{
+                        outputStream.write(command.getBytes());
+                    }
+                    catch (IOException e){
+                        e.printStackTrace();
+                    }
+                }
+
+                else if (result.get(0).equalsIgnoreCase("sağ")){
+                    command = "4";
+                    try{
+                        outputStream.write(command.getBytes());
+                    }
+                    catch (IOException e){
+                        e.printStackTrace();
+                    }
+                }
+
+                else if (result.get(0).equalsIgnoreCase("aç")){
+                    command = "5";
+                    try{
+                        outputStream.write(command.getBytes());
+                    }
+                    catch (IOException e){
+                        e.printStackTrace();
+                    }
+                }
+
+                else if (result.get(0).equalsIgnoreCase("kapat")){
+                    command = "6";
+                    try{
+                        outputStream.write(command.getBytes());
+                    }
+                    catch (IOException e){
+                        e.printStackTrace();
+                    }
+                }
+
+
+
+                else{
+                    command = "10";
+                    try{
+                        outputStream.write(command.getBytes());
+                    }
+                    catch(IOException e){
+                        e.printStackTrace();
+                    }
+                }
+
             }
         }
     }
